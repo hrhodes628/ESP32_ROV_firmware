61ad7de6652b020965c09e4ca7c8452448b0c7cc
hrhodes628 <harryrhodes2005@gmail.com>
2025-12-16 21:36:32 +0000

    configure main for init

--- sub/main/main.c
+++ sub/main/main.c
@@ -2,9 +2,20 @@
 #include "ibus.h"
 #include "telemetry.h"
 #include "motor.h"
+#include "control.h"
 
 
 void app_main(void)
 {
+    motor_init();
 
+    ibus_config_t ibus_cfg = {
+            .uart=IBUS_UART,
+            .tx_pin=IBUS_TXD_PIN,
+            .rx_pin=IBUS_RXD_PIN,
+            .channel_count=8
+    };
+    ibus_init(&ibus_cfg);
+    control_init();
+    telemetry_init();
 }
